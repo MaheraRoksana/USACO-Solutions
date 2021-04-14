185525164ffe5f9e7c7a128e0df22cea34a8385b
MaheraRoksana <maherarislam@gmail.com>
2021-04-14 17:33:11 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*
+ID: maherar1
+PROG: ride
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("ride.out");
+    ifstream fin ("ride.in");
+    //freopen("ride.in", "r", stdin);
+    //freopen("ride.out", "w", stdout);
+    string c, g;
+    int sc=1;
+    int sg = 1;
+    fin >> c >> g;
+    for(int i =0; i<c.size();i++){
+        sc *= (int)c[i]-64;
+    }
+    for(int i =0; i<g.size();i++){
+        sg *= (int)g[i]-64;
+    }
+    sc = sc%47;
+    sg = sg%47;
+    if(sc==sg){
+        fout<<"GO"<<endl;
+    }else{
+        fout<<"STAY"<<endl;
+    }
+    return 0;
+}
